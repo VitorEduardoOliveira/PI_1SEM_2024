a6ea5446794e703216b451853390c66a12ad9324
Vitor Eduardo de Oliveira <132387358+VitorEduardoOliveira@users.noreply.github.com>
2024-05-25 10:42:39 -0300

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpResponse
+from django.urls import reverse
+from core.forms import ONGForm
+from core.models import ONGModel
+
+def index(request):
+  if request.method == "GET":
+    return render(request, "index.html")
+  return HttpResponseRedirect(reverse('core:index'))
+
+def doe(request):
+  if request.method == "GET":
+    return render(request, "doe.html")
+  return HttpResponseRedirect(reverse('core:doe'))
+
+def nossotime(request):
+  if request.method == "GET":
+    return render(request, "nossotime.html")
+  return HttpResponseRedirect(reverse('core:nossotime'))
+
+def contato(request):
+  if request.method == "GET":
+    return render(request, "contato.html")
+  return HttpResponseRedirect(reverse('core:contato'))
+
+def loginONG(request):
+  if request.method == "POST":
+    form = ONGForm(request.POST)
+    if form.is_valid():
+      ONGModel.objects.create(**form.cleaned_data)
+      return HttpResponseRedirect(reverse('core:login'))
+    contexto = {'form': form}
+    return render(request, "login.html", contexto)
+  contexto = {'form': ONGForm()}
+  return render(request, "login.html", contexto)
